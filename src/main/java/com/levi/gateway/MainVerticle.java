d5b209b5eb7dc20ee97742d1ec849aff093d4ba6
liuwenqiang <wenqiang.liu@wenge.com>
2025-03-13 16:35:22 +0800

    init project

---
+++ src/main/java/com/levi/gateway/MainVerticle.java
@@ -0,0 +1,18 @@
+package com.levi.gateway;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new MainVerticle());
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        System.out.println("MainVerticle started");
+    }
+}
