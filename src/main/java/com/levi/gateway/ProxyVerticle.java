556b214fa11af316320a822f437b1f8eff4d7a1f
liuwenqiang <wenqiang.liu@wenge.com>
2025-03-14 18:07:17 +0800

    支持post的反向代理

--- src/main/java/com/levi/gateway/ProxyVerticle.java
+++ src/main/java/com/levi/gateway/ProxyVerticle.java
@@ -2,7 +2,9 @@ package com.levi.gateway;
 
 import com.levi.gateway.constant.NetConstant;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
 import io.vertx.core.http.*;
+import io.vertx.ext.web.handler.BodyHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,9 +16,13 @@ public class ProxyVerticle extends AbstractVerticle {
         HttpServer server = vertx.createHttpServer();
 
         // 构建发送去目标服务的客户端
-        HttpClientOptions httpClientOptions = new HttpClientOptions();
-        httpClientOptions.setDefaultHost(NetConstant.LOCALHOST);
-        httpClientOptions.setDefaultPort(NetConstant.HTTP_PORT);
+        HttpClientOptions httpClientOptions = new HttpClientOptions()
+                .setDefaultHost("127.0.0.1")
+                .setDefaultPort(8080)
+//                .setDefaultPort(443)
+//               .setSsl(true)
+//               .setTrustAll(true)
+                .setKeepAlive(true);
         HttpClient httpClient = vertx.createHttpClient(httpClientOptions);
 
         // 8081端口的请求走requestHandler处理器
@@ -25,22 +31,31 @@ public class ProxyVerticle extends AbstractVerticle {
             HttpServerResponse response = sourceRequest.response();
             HttpMethod method = sourceRequest.method();
             String uri = sourceRequest.uri();
+            MultiMap headers = sourceRequest.headers();
+
             if (method == HttpMethod.GET) {
                 // 拿到代理请求，转发目标服务，注意这里request是构建请求对象，真实发送请求在下面的send中
                 httpClient.request(method, uri, createReq -> {
                     if (createReq.succeeded()) {
                         HttpClientRequest request = createReq.result();
-                        // 代理发送数据去目标服务
-                        request.send(targetReps -> {
-                            // 拿到目标服务的响应，返回给客户端
-                            if (targetReps.succeeded()) {
-                                HttpClientResponse targetResponseResult = targetReps.result();
-                                targetResponseResult.bodyHandler(body -> {
-                                    response.setStatusCode(targetResponseResult.statusCode());
-                                    response.end(body.toString());
-                                });
+                        headers.forEach(header -> {
+                            if ("Content-Type".equals(header.getKey())) {
+                                request.putHeader(header.getKey(), header.getValue());
                             }
                         });
+                        // 代理发送数据去目标服务
+                        request.send()
+                                .onSuccess(targetReps ->{
+                                    // 拿到目标服务的响应，返回给客户端
+                                    targetReps.bodyHandler(body -> {
+                                        response.setStatusCode(targetReps.statusCode());
+                                        response.end(body.toString());
+                                    });
+                                })
+                                .onFailure(err ->{
+                                    err.printStackTrace();
+                                    response.setStatusCode(500).end(err.getMessage());
+                                });
                     }
                 });
             }
